c7ae6040d24c30c1ba2caecaa5218cfc4453a280
cmpo86 <cmpochumphon@gmail.com>
2020-06-02 15:34:39 +0700

    modified template dashboard

--- covid19/covid/urls.py
+++ covid19/covid/urls.py
@@ -1,9 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import Home, DashBoard
+from .views import *
 
 urlpatterns = [
     path('', Home, name='home-page'),
-    path('dashboard/', DashBoard, name='dashboard'),
+    path('dashboard/', DashBoard, name='dashboard-page'),
+    path('icons/', Icons, name='icons-page'),
+    path('tables/', Tables, name='tables-page'),
+    path('user/', User, name='user-page'),
+    path('typography/', Typography, name='typography-page')
 ]
--- covid19/covid/views.py
+++ covid19/covid/views.py
@@ -5,3 +5,15 @@ def Home(request):
 
 def DashBoard(request):
     return render(request, 'covid/dashboard.html')
+
+def Icons(request):
+    return render(request, 'covid/icons.html')
+
+def Tables(request):
+    return render(request, 'covid/tables.html')
+
+def User(request):
+    return render(request, 'covid/user.html')
+
+def Typography(request):
+    return render(request, 'covid/typography.html')
